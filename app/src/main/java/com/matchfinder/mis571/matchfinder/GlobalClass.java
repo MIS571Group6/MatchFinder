f70377d8cd2dc13747973dedc5f0b7f917eebfd8
manuel <mklauss@wpi.edu>
2018-10-28 20:56:03 -0400

    -

---
+++ app/src/main/java/com/matchfinder/mis571/matchfinder/GlobalClass.java
@@ -0,0 +1,6 @@
+package com.matchfinder.mis571.matchfinder;
+
+import android.app.Application;
+
+public class GlobalClass extends Application {
+}
--- app/src/main/java/com/matchfinder/mis571/matchfinder/LogIn.java
+++ app/src/main/java/com/matchfinder/mis571/matchfinder/LogIn.java
@@ -2,6 +2,9 @@ package com.matchfinder.mis571.matchfinder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LogIn extends AppCompatActivity {
 
@@ -11,9 +14,19 @@ public class LogIn extends AppCompatActivity {
         setContentView(R.layout.activity_log_in);
 
 
+        Button logInBtn = (Button) findViewById(R.id.logInBtn);
 
-        
+        logInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+
+                EditText emailEditText = (EditText) findViewById(R.id.emailEditText);
+                EditText passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+
+
+            }
+        });
 
 
 
